3895040669434472d2948cefc01cea708baba6fe
nyitraibalazs <nyitraibalazs.h@gmail.com>
2019-04-28 10:01:47 +0200

    Kacsinak

--- src/app/service.ts
+++ src/app/service.ts
@@ -1,31 +1,39 @@
 export class PractiseServiceComponent {
-/* tslint:disable */
+  /* tslint:disable */
   EmberiIntelligencia() {
     const myArr = [
       {
         "id": 1,
-        "name": "Mekkora az IQ értéke annak a két tízéves iskolásnak, akiknek az MK-ÉK differenciája +2 és -2?",
+        "nev": "Mekkora az IQ értéke annak a két tízéves iskolásnak, akiknek az MK-ÉK differenciája +2 és -2?",
         "pontszam": "6",
+        "resz": "1",
       },
       {
         "id": 2,
-        "name": "Mit értünk az IQ tesztek megbízhatóságán és érvényességén?",
+        "nev": "Mit értünk az IQ tesztek megbízhatóságán és érvényességén?",
         "pontszam": "2",
+        "resz": "1",
       },
       {
         "id": 3,
-        "name": "Mire szolgál a Turing teszt és hogy működik?",
+        "nev": "Mire szolgál a Turing teszt és hogy működik?",
         "pontszam": "3",
+        "resz": "1",
+
       },
       {
         "id": 4,
-        "name": "Adja meg az inter- és az intraperszonális intelligencia jelentését!",
+        "nev": "Adja meg az inter- és az intraperszonális intelligencia jelentését!",
         "pontszam": "3",
+        "resz": "1",
+
       },
       {
         "id": 5,
-        "name": "Ismertesse Descartes-nek a módszeres gondolkodásra vonatkozó tételeit!",
+        "nev": "Ismertesse Descartes-nek a módszeres gondolkodásra vonatkozó tételeit!",
         "pontszam": "4",
+        "resz": "1",
+
       }];
     return myArr;
   }
@@ -34,28 +42,33 @@ export class PractiseServiceComponent {
     const myArr1 = [
       {
         "id": 1,
-        "name": "Milyen közös összekötő található az AI elnevezéshez és a LISP programozási nyelvhez?",
+        "nev": "Milyen közös összekötő található az AI elnevezéshez és a LISP programozási nyelvhez?",
         "pontszam": "2",
+        "resz": "1",
       },
       {
         "id": 2,
-        "name": "Adja meg az ágens bővebb értelmezését!",
+        "nev": "Adja meg az ágens bővebb értelmezését!",
         "pontszam": "3",
+        "resz": "1",
       },
       {
         "id": 3,
-        "name": "Milyen súlypontáthelyeződés figyelhető meg napjainkban a tudásszemléltetés alkalmazot módszereiben, céljában?",
+        "nev": "Milyen súlypontáthelyeződés figyelhető meg napjainkban a tudásszemléltetés alkalmazot módszereiben, céljában?",
         "pontszam": "3",
+        "resz": "1",
       },
       {
         "id": 4,
-        "name": "Hogyan működnek a racionális ágensek?",
+        "nev": "Hogyan működnek a racionális ágensek?",
         "pontszam": "3",
+        "resz": "1",
       },
       {
         "id": 5,
-        "name": "Milyen tulajdonságai alapján nevezhetünk egy hűtőgépet ágensnek?",
+        "nev": "Milyen tulajdonságai alapján nevezhetünk egy hűtőgépet ágensnek?",
         "pontszam": "3",
+        "resz": "1",
       }];
     return myArr1;
   }
@@ -65,28 +78,33 @@ export class PractiseServiceComponent {
     const myArr2 = [
       {
         "id": 1,
-        "name": "27. Sorolja fel a mesterséges intelligencia 14 alkalmazási területét!",
+        "nev": "27. Sorolja fel a mesterséges intelligencia 14 alkalmazási területét!",
         "pontszam": "7",
+        "resz": "1",
       },
       {
         "id": 2,
-        "name": "Adjon példát egy minimax fagráfra, amelyen 2 alfa és 4 béta lenyesés van!",
+        "nev": "Adjon példát egy minimax fagráfra, amelyen 2 alfa és 4 béta lenyesés van!",
         "pontszam": "5",
+        "resz": "1",
       },
       {
         "id": 3,
-        "name": "Mire szolgál az adatbányászat, a szimbolikus számítás és a beszédfeldolgozás?",
+        "nev": "Mire szolgál az adatbányászat, a szimbolikus számítás és a beszédfeldolgozás?",
         "pontszam": "6",
+        "resz": "1",
       },
       {
         "id": 4,
-        "name": "Mi a célja a szimbolikus számításnak ?",
+        "nev": "Mi a célja a szimbolikus számításnak ?",
         "pontszam": "3",
+        "resz": "1",
       },
       {
         "id": 5,
-        "name": "Adja meg a szakértőrendszer definícióját!",
+        "nev": "Adja meg a szakértőrendszer definícióját!",
         "pontszam": "4",
+        "resz": "1",
       }];
     return myArr2;
   }
@@ -95,28 +113,33 @@ export class PractiseServiceComponent {
     const myArr3 = [
       {
         "id": 1,
-        "name": "Mi a tudáskinyerési folyamat négy fő szakasza?",
+        "nev": "Mi a tudáskinyerési folyamat négy fő szakasza?",
         "pontszam": "4",
+        "resz": "1",
       },
       {
         "id": 2,
-        "name": "Mi a lényege a protokol elemzésnek?",
+        "nev": "Mi a lényege a protokol elemzésnek?",
         "pontszam": "2",
+        "resz": "1",
       },
       {
         "id": 3,
-        "name": "Milyen tudáskinyerési technikák léteznek? (csak felsorolás)",
+        "nev": "Milyen tudáskinyerési technikák léteznek? (csak felsorolás)",
         "pontszam": "4",
+        "resz": "1",
       },
       {
         "id": 4,
-        "name": "Mi a tudástechnológus feladata a tudáskinyerés kapcsán?",
+        "nev": "Mi a tudástechnológus feladata a tudáskinyerés kapcsán?",
         "pontszam": "3",
+        "resz": "1",
       },
       {
         "id": 5,
-        "name": "Adjon meg hat szabályt és néhány tényt és mutassa be velük a hátraláncoló szabályalkalmazás működését",
+        "nev": "Adjon meg hat szabályt és néhány tényt és mutassa be velük a hátraláncoló szabályalkalmazás működését",
         "pontszam": "6",
+        "resz": "1",
       }];
     return myArr3;
   }
@@ -125,63 +148,75 @@ export class PractiseServiceComponent {
     const myArr4 = [
       {
         "id": 1,
-        "name": "Adja meg a propozíciós logika célját, szintaktikájának és szemantikájának összetevőit!",
+        "nev": "Adja meg a propozíciós logika célját, szintaktikájának és szemantikájának összetevőit!",
         "pontszam": "8",
+        "resz": "1",
       },
       {
         "id": 2,
-        "name": "Adja meg a rezolúció - formailag tört alakban megadott - szabályát és értelmezze!",
+        "nev": "Adja meg a rezolúció - formailag tört alakban megadott - szabályát és értelmezze!",
         "pontszam": "3",
+        "resz": "1",
       },
       {
         "id": 3,
-        "name": "Mutassa be a fuzzy logika működését egy, az előadáson mutatott példára hasonlító,\n" +
+        "nev": "Mutassa be a fuzzy logika működését egy, az előadáson mutatott példára hasonlító,\n" +
           "de nem azonos példával!",
         "pontszam": "6",
+        "resz": "1",
       },
       {
         "id": 4,
-        "name": "Ismertesse a propozíciós logika célját és szintaxisának elemeit!",
+        "nev": "Ismertesse a propozíciós logika célját és szintaxisának elemeit!",
         "pontszam": "6",
+        "resz": "1",
       },
       {
         "id": 5,
-        "name": "Ismertesse a predikátum logika szintaktikáját és szemantikáját részletesen!",
+        "nev": "Ismertesse a predikátum logika szintaktikáját és szemantikáját részletesen!",
         "pontszam": "10",
+        "resz": "1",
       }];
     return myArr4;
   }
 
+
+  //2. ZH
   Prolog() {
     const myArr5 = [
       {
         "id": 1,
-        "name": "Ismertesse a PLANNER részeit és működését!",
+        "nev": "Ismertesse a PLANNER részeit és működését!",
         "pontszam": "6",
+        "resz": "2",
       },
       {
         "id": 2,
-        "name": " Ismertesse a Planner újdonságait!",
+        "nev": " Ismertesse a Planner újdonságait!",
         "pontszam": "3",
+        "resz": "2",
       },
       {
         "id": 3,
-        "name": "Irjon Prolog programot egy számsorozat szorzatának meghatározására!",
+        "nev": "Irjon Prolog programot egy számsorozat szorzatának meghatározására!",
         "pontszam": "6",
+        "resz": "2",
       },
       {
         "id": 4,
-        "name": "Mutassa be a Prolog működését fagráffal a   \n" +
+        "nev": "Mutassa be a Prolog működését fagráffal a   \n" +
           "A Föld nagyobb, mint a Hold.\n" +
           "A Mars nagyobb, mint a Föld.\n" +
           "Melyik bolygó közepes?\n" +
           "feladatot megoldó Prolog programon! \n",
         "pontszam": "6",
+        "resz": "2",
       },
       {
         "id": 5,
-        "name": "107.\tAdjon példát olyan LISP listákra, melyek megadják egy nényelemű lista utolsó elemét, kétféle módon!",
+        "nev": "107.\tAdjon példát olyan LISP listákra, melyek megadják egy nényelemű lista utolsó elemét, kétféle módon!",
         "pontszam": "4",
+        "resz": "2",
       }];
     return myArr5;
   }
@@ -190,30 +225,35 @@ export class PractiseServiceComponent {
     const myArr6 = [
       {
         "id": 1,
-        "name": "Rajzolja meg az előadáson mutatott New York-i metrós szállításhoz hasonló szemantikus háló ábrán a következő viszonylatokat:\n" +
+        "nev": "Rajzolja meg az előadáson mutatott New York-i metrós szállításhoz hasonló szemantikus háló ábrán a következő viszonylatokat:\n" +
           "A tolvaj szilveszter éjjelén lopta el a Louvre-ból Belfegor álarcát.      \n",
         "pontszam": "6",
+        "resz": "2",
       },
       {
         "id": 2,
-        "name": "Mutassa be példákkal a szemantikus hálók problémáit!",
+        "nev": "Mutassa be példákkal a szemantikus hálók problémáit!",
         "pontszam": "6",
+        "resz": "2",
       },
       {
         "id": 3,
-        "name": "Adja meg azt a szemantikus hálót, amelyik a következő viszonylatokat szemlélteti:\n" +
+        "nev": "Adja meg azt a szemantikus hálót, amelyik a következő viszonylatokat szemlélteti:\n" +
           "A művészek is emberek. A táncdalénekesek olyan művészek, akik jól tudnak énekelni. Koncz Zsuzsa egy hosszú hajú táncdalénekes\n",
         "pontszam": "4",
+        "resz": "2",
       },
       {
         "id": 4,
-        "name": "Adja meg a szemantikus háló alapú tudásszemléltetés hibáit!",
+        "nev": "Adja meg a szemantikus háló alapú tudásszemléltetés hibáit!",
         "pontszam": "4",
+        "resz": "2",
       },
       {
         "id": 5,
-        "name": "Vesse össze a keretek, a script-ek és a sémák ismeretábrázolását! Hasonlóságok, eltérések.",
+        "nev": "Vesse össze a keretek, a script-ek és a sémák ismeretábrázolását! Hasonlóságok, eltérések.",
         "pontszam": "9",
+        "resz": "2",
       }];
     return myArr6;
   }
@@ -222,28 +262,33 @@ export class PractiseServiceComponent {
     const myArr7 = [
       {
         "id": 1,
-        "name": "Rajzoljon egy 6 városból álló térképet és mutassa be rajta az elágaztatás és ugrálás (Branch and bound) és a lokális informáltságú hegymászó keresés működését! Az útszakaszok irányított nyilak legyenek! ",
+        "nev": "Rajzoljon egy 6 városból álló térképet és mutassa be rajta az elágaztatás és ugrálás (Branch and bound) és a lokális informáltságú hegymászó keresés működését! Az útszakaszok irányított nyilak legyenek! ",
         "pontszam": "10",
+        "resz": "2",
       },
       {
         "id": 2,
-        "name": "Sorolja fel a lokális kereső algoritmusokat és ismertesse a Tabu keresés működési elvét! ",
+        "nev": "Sorolja fel a lokális kereső algoritmusokat és ismertesse a Tabu keresés működési elvét! ",
         "pontszam": "3",
+        "resz": "2",
       },
       {
         "id": 3,
-        "name": "Rajzoljon egy 6 városból álló térképet és mutassa be rajta az elágaztatás és ugrálás (Branch and bound) és a lokális informáltságú hegymászó keresés működését! Az útszakaszok irányított nyilak legyenek! ",
+        "nev": "Rajzoljon egy 6 városból álló térképet és mutassa be rajta az elágaztatás és ugrálás (Branch and bound) és a lokális informáltságú hegymászó keresés működését! Az útszakaszok irányított nyilak legyenek! ",
         "pontszam": "7",
+        "resz": "2",
       },
       {
         "id": 4,
-        "name": "Mire szolgál a heurisztikus kiértékelő függvény a keresésnél?  (3p) Mit értünk a keresés idő- és memória-komplexitása alatt?",
+        "nev": "Mire szolgál a heurisztikus kiértékelő függvény a keresésnél?  (3p) Mit értünk a keresés idő- és memória-komplexitása alatt?",
         "pontszam": "4",
+        "resz": "2",
       },
       {
         "id": 5,
-        "name": "Melyik találja meg az optimumot gyorsabban: a hegymászó keresés, vagy a genetikus algoritmus? Indoklással!",
+        "nev": "Melyik találja meg az optimumot gyorsabban: a hegymászó keresés, vagy a genetikus algoritmus? Indoklással!",
         "pontszam": "5",
+        "resz": "2",
       }];
     return myArr7;
   }
@@ -252,13 +297,15 @@ export class PractiseServiceComponent {
     const myArr8 = [
       {
         "id": 1,
-        "name": "Ismertesse a genetikus algoritmust! Mit jelent az allél?",
+        "nev": "Ismertesse a genetikus algoritmust! Mit jelent az allél?",
         "pontszam": "5",
+        "resz": "2",
       },
       {
         "id": 2,
-        "name": "Adja meg az evolúciós algoritmus lépéseit!",
+        "nev": "Adja meg az evolúciós algoritmus lépéseit!",
         "pontszam": "4",
+        "resz": "2",
       }];
     return myArr8;
   }
@@ -267,30 +314,35 @@ export class PractiseServiceComponent {
     const myArr9 = [
       {
         "id": 1,
-        "name": "Hol találkozott csapokkal és pálcikákkal a tananyagban? Mi a feladatuk?",
+        "nev": "Hol találkozott csapokkal és pálcikákkal a tananyagban? Mi a feladatuk?",
         "pontszam": "1",
+        "resz": "2",
       },
       {
         "id": 2,
-        "name": "Adja meg az agy központi magjának részeit és feladatukat!",
+        "nev": "Adja meg az agy központi magjának részeit és feladatukat!",
         "pontszam": "7",
+        "resz": "2",
       },
       {
         "id": 3,
-        "name": "Ismertesse a pszichológia kognitív irányzatát megelőző irányzatait? Mi a retikuláris rendszer szerepe?",
+        "nev": "Ismertesse a pszichológia kognitív irányzatát megelőző irányzatait? Mi a retikuláris rendszer szerepe?",
         "pontszam": "3",
+        "resz": "2",
       },
       {
         "id": 4,
-        "name": "Ismertesse a felejtés működését, kutatóját nevezze meg!",
+        "nev": "Ismertesse a felejtés működését, kutatóját nevezze meg!",
         "pontszam": "4",
+        "resz": "2",
       },
       {
         "id": 5,
-        "name": "Mutassa be ugyanazon a 6 várost tartalmazó hálós gráfon a keresőgráfok megrajzolásával az\n" +
+        "nev": "Mutassa be ugyanazon a 6 várost tartalmazó hálós gráfon a keresőgráfok megrajzolásával az\n" +
           "elágazás és ugrálás (Branch &amp; Bound) és a legjobbat először (mohó, Best first) kereső\n" +
           "algoritmusokat!",
         "pontszam": "12",
+        "resz": "2",
       }];
     return myArr9;
   }
@@ -299,32 +351,34 @@ export class PractiseServiceComponent {
     const myArr10 = [
       {
         "id": 1,
-        "name": "Ismertesse az ART háló felépítését és működésének lépéseit!",
+        "nev": "Ismertesse az ART háló felépítését és működésének lépéseit!",
         "pontszam": "9",
+        "resz": "2",
       },
       {
         "id": 5,
-        "name": "Ismertesse a Kohonen-féle önszervező hálót rajzzal, szöveggel!",
+        "nev": "Ismertesse a Kohonen-féle önszervező hálót rajzzal, szöveggel!",
         "pontszam": "7",
+        "resz": "2",
       },
       {
         "id": 3,
-        "name": "Adja meg a mesterséges neuronok átviteli függvényeinek típusait!",
+        "nev": "Adja meg a mesterséges neuronok átviteli függvényeinek típusait!",
         "pontszam": "4",
+        "resz": "2",
       },
       {
         "id": 4,
-        "name": "Ismertesse a Hopfield háló felépítését és működésének lépéseit!",
+        "nev": "Ismertesse a Hopfield háló felépítését és működésének lépéseit!",
         "pontszam": "9",
+        "resz": "2",
       },
       {
         "id": 5,
-        "name": "Ismertesse a multilayer perceptron előnyét, működését, tanítási módszerét!",
+        "nev": "Ismertesse a multilayer perceptron előnyét, működését, tanítási módszerét!",
         "pontszam": "6",
+        "resz": "2",
       }];
     return myArr10;
   }
-
-
-
 }
